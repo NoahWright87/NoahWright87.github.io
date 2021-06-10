886daba9233f76d01d212aead479200cf9d73ce1
Noah Wright <noahwright87@gmail.com>
2021-06-10 15:38:55 -0500

    Contact Me form is now functional.  Cleaned up readme

--- readme.md
+++ readme.md
@@ -1,44 +1,16 @@
-﻿# NoahWright.dev Site
-This repo holds the code used to build the site at NoahWright.dev.  A tutorial for getting your own React-based site hosted on GitHub can be found here: https://github.com/gitname/react-gh-pages
+﻿# Noah Wright's Developer Site
+This repo holds the code used to build the site at [NoahWright.dev](https://noahwright.dev/).  This is both my own area to play around with web technologies and a way to show off the things I've been working on.
 
-# Features
-As I add features, I will list them here.
-- React + Bootstrap site, hosted on GitHub
+## 💻 Technology Used
+- React + Bootstrap site, hosted via [GitHub Pages](https://github.com/gitname/react-gh-pages)
 - Resume generated from JSON file
+- Axios to call an ASP.NET webservice to email/text me
 
-# Work in Progress
-I'm working on this in my (very limited) spare time, so pardon the dust.  Here are the changes coming down the road.
+## 🚧 Work in Progress
+I have a full time software engineer job, I'm in the Air Force Reserves, and I have a wife and kids.  I don't have a ton of free time, but when I do, I tinker with this site.  Check back occasionally to see what I've been up to!
 
-## 🥳 Recent Changes
-Current:
+### 🥳 Most Recent Changes
+From newest to oldest:
+- Contact Me form now works, will send an email and text to me without revealing my phone number/email address.
+- Removed the *Future Changes* section below, added those as Issues on GitHub.
 - Update Portfolio page to have some old Unity-based games and my current GitHub project: Level Up Learning.
-
-## ⌚ Future Changes
-Some of the below will be selected for the next commit once I'm done with the above:
-- A better favicon that isn't just my initials.
-- Portfolio should have pictures of things
-- Contact - make form actually function
-- Parallax background somewhere, because they're cool 😎
-- A color scheme of some sort
-- Class for "skill icons"
-  - Can be used throughout the site to indicate proficiency or what a project uses just be inserting a tag
-  - Each skill needs an icon in at least two sizes (a scaleable SVG would be preferred)
-  - Tapping / hovering should bring up a tooltip explaining why the icon is there (e.g.: "Site was written in React", "C# Backend")
-- Class for "how's it done"
-  - Anywhere something special is done on the site (special code written, outside services called, etc) have a common callout that can be clicked / hovered over to explain what's being done
-- Movement / Animations
-  - Color change when hovering over things
-  - When going from page to page
-  - When first landing on the page
-  - Things appear as you scroll - resume is a good spot for that
-  - Cycling through images in a spot or two
-  - Something should pop up when scrolled all the way down
-- Google Analytics
-
-## 🏁 Changes Coming Eventually (maybe)
-These are my more lofty goals, such as:
-- Light/dark mode selectable in top navigation
-- JSON-based resume improvements
-  - Create an interface to enter resume details and create JSON
-  - Eventually: have that JSON file served up by a cloud database
-- Ongoing: add more projects to the Portfolio section
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import BaseComponent from './BaseComponent'
-/*import { Formik, Form, Field, ErrorMessage, } from 'formik';*/
-import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Formik, Form, Field, ErrorMessage, } from 'formik';
+import axios from 'axios';
+//import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
+//TODO: Switching back to Formik made this ugly again - need to make it prettier
 
 export class Contact extends BaseComponent {
     static displayName = Contact.name;
@@ -11,83 +13,83 @@ export class Contact extends BaseComponent {
         return (
             <div>
                 <h1>Contact Noah</h1>
-                <p><strong>NOTE:</strong> This is a non-functioning placeholder that will later be used to send me a message.</p>
-                <Form>
-                    <FormGroup row>
-                        <Label for="name" sm={2}>Name</Label>
-                        <Col sm={10}>
-                            <Input type="text" name="name" id="name" placeholder="Your name" />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup row>
-                        <Label for="email" sm={2}>Email</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="email" placeholder="your.name@mail.com" />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="message">Message</Label>
-                        <Input type="textarea" name="message" id="message" />
-                    </FormGroup>
-                    <Button onClick={() => alert("Eventually, this will send me an email, text, tweet, DM, wuphf, etc") }>Submit</Button>
-                </Form>
 
+                <Formik
+                    initialValues={{ email: '', name: '', message: '' }}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.name) {
+                            errors.name = 'Required';
+                        }
 
-                {/*<Formik*/}
-                {/*    initialValues={{ email: '', name: '', message: '' }}*/}
-                {/*    validate={values => {*/}
-                {/*        const errors = {};*/}
-                {/*        if (!values.name) {*/}
-                {/*            errors.name = 'Required';*/}
-                {/*        }*/}
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Invalid email address';
+                        }
 
-                {/*        if (!values.email) {*/}
-                {/*            errors.email = 'Required';*/}
-                {/*        } else if (*/}
-                {/*            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)*/}
-                {/*        ) {*/}
-                {/*            errors.email = 'Invalid email address';*/}
-                {/*        }*/}
-
-                {/*        if (!values.message) {*/}
-                {/*            errors.message = 'Required';*/}
-                {/*        }*/}
-                {/*        return errors;*/}
-                {/*    }}*/}
-                {/*    onSubmit={(values, { setSubmitting }) => {*/}
-                {/*        setTimeout(() => {*/}
-                {/*            //alert(JSON.stringify(values, null, 2));*/}
-                {/*            alert("//TODO: Send Noah an email, text, tweet, DM, wuphf, etc");*/}
-                {/*            setSubmitting(false);*/}
-                {/*        }, 400);*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*    {({ isSubmitting }) => (*/}
-                {/*        <Form>*/}
-                {/*            <div>*/}
-                {/*                <label htmlFor="name">Name</label>*/}
-                {/*                <Field type="text" name="name" />*/}
-                {/*                <ErrorMessage name="name" component="div" />*/}
-                {/*            </div>*/}
-                {/*            <div>*/}
-                {/*                <label htmlFor="email">Email Address</label>*/}
-                {/*                <Field type="email" name="email" />*/}
-                {/*                <ErrorMessage name="email" component="div" />*/}
-                {/*            </div>*/}
-                {/*            <div>*/}
-                {/*                <label htmlFor="message">Message to Send</label><br />*/}
-                {/*                <Field type="text" as="textarea" name="message" />*/}
-                {/*                <ErrorMessage name="message" component="div" />*/}
-                {/*            </div>*/}
-                {/*            <div>*/}
-                {/*                <button type="submit" disabled={isSubmitting}>*/}
-                {/*                    Submit*/}
-                {/*                </button>*/}
-                {/*            </div>*/}
-                {/*        </Form>*/}
-                {/*    )}*/}
-                {/*</Formik>*/}
+                        if (!values.message) {
+                            errors.message = 'Required';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            this.handleSubmit(values);
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <div>
+                                <label htmlFor="name">Name</label>
+                                <Field type="text" name="name" />
+                                <ErrorMessage name="name" component="div" />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email Address</label>
+                                <Field type="email" name="email" />
+                                <ErrorMessage name="email" component="div" />
+                            </div>
+                            <div>
+                                <label htmlFor="message">Message to Send</label><br />
+                                <Field type="text" as="textarea" name="message" />
+                                <ErrorMessage name="message" component="div" />
+                            </div>
+                            <div>
+                                <button type="submit" disabled={isSubmitting}>
+                                    Submit
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+                {/*TODO: Have a success message appear here*/}
             </div>
         );
+    }
+
+    handleSubmit(args) {
+        var request = {
+            name: args.name,
+            email: args.email,
+            message: args.message
+        };
+
+        var url = 'https://noahwrightwebservices.azurewebsites.net/ContactMe/Send';
+        axios.post(url, request,
+        )
+            .then(response => {
+                alert("Thanks for reaching out!  I'll be in touch!");
+            })
+            .catch(err => {
+                alert("Error occurred while sending the message - please try again later!");
+            })
+            .finally(() => {
+                //Clean-up would go here
+            });
     }
 }
